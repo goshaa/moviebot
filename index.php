ac9cf8c0aff5c5fdaaf17a04df0d878139f3f79a
Klizzy <steven.zemelka@gmail.com>
2017-06-27 21:45:02 +0200

    fucking init commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require "vendor/autoload.php";
+use Mpociot\BotMan\BotManFactory;
+use Mpociot\BotMan\BotMan;
+
+$config = [
+    'telegram_token' => '391088343:AAHp8tEBheJSC9TQ-VB6WPxLZolPru-ZMKw',
+];
+
+echo "<pre>";
+var_dump('asda');
+echo "</pre>";
+die();
+
+// create an instance
+$botman = BotManFactory::create($config);
+
+// give the bot something to listen for.
+$botman->hears('hello', function (BotMan $bot) {
+    $bot->reply('Hello yourself.');
+});
+
+// start listening
+$botman->listen();
